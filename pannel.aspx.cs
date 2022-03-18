c770855ec4084153e4c56f1d0bacf11ff9223d73
PrathameshPVM <95204156+PrathameshPVM@users.noreply.github.com>
2022-03-19 02:28:13 +0530

    Add files via upload

---
+++ pannel.aspx.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace pannel
+{
+    public partial class pannel : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            Panel1.Visible = true;
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+           Panel1.Visible=false;
+        }
+
+        protected void Button3_Click(object sender, EventArgs e)
+        {
+            Label1.Text = "Name : " + TextBox1.Text;
+            Label2.Text = "Pass : " + TextBox2.Text;
+        }
+
+        
+    }
+}
