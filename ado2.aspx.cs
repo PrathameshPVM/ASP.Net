fefd4b9c77b70becc52c2ecf51bdcc4e9ca1c087
PrathameshPVM <95204156+PrathameshPVM@users.noreply.github.com>
2022-03-19 21:52:53 +0530

    Add files via upload

--- ado2.aspx.cs
+++ ado2.aspx.cs
@@ -15,16 +15,18 @@ namespace ado1
         {
 
         }
+        static string cn = "data source = LAPTOP-BG65F1TL\\SQLEXPRESS;Integrated Security=true;database=ado";
 
+        SqlConnection con = new SqlConnection(cn);
+        SqlCommand command;
+        string qr;
         protected void Button1_Click(object sender, EventArgs e)
         {
-            string cn = "data source = LAPTOP-BG65F1TL\\SQLEXPRESS;Integrated Security=true;database=ado";
-
-            SqlConnection con = new SqlConnection(cn);
+            
             try
             {
-                string qr = "insert into stu(rno,name) values('"+TextBox1.Text+"','"+TextBox2.Text+"')";
-                SqlCommand command = new SqlCommand(qr, con);
+                qr = "insert into stu(rno,name) values('"+TextBox1.Text+"','"+TextBox2.Text+"')";
+                command = new SqlCommand(qr, con);
                 con.Open();
                 command.ExecuteNonQuery();
                 Label1.Text = "Record Saved Successfully";
@@ -35,6 +37,29 @@ namespace ado1
             catch (Exception ee)
             {
                 Label1.Text=ee.ToString();
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                qr = "update stu set name='" + TextBox2.Text + "'where rno='"+TextBox1.Text+"'";
+                command = new SqlCommand(qr, con);
+                con.Open();
+                command.ExecuteNonQuery();
+                Label1.Text = "Record Updated Successfully";
+                TextBox1.Text = "";
+                TextBox2.Text = "";
+                TextBox1.Focus();
+            }
+            catch (Exception ee)
+            {
+                Label1.Text = ee.ToString();
             }
             finally
             {
