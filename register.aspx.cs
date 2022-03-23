820fbefcb76142e24036fe93f1ceef7060813519
PrathameshPVM <95204156+PrathameshPVM@users.noreply.github.com>
2022-03-23 09:16:36 +0530

    Add files via upload

---
+++ register.aspx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace state_management
+{
+    public partial class WebForm2 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+
+            HttpCookie cookie1 = new HttpCookie("c");
+            cookie1.Values.Add("Username", TextBox1.Text);
+            cookie1.Values.Add("Email", TextBox2.Text);
+            cookie1.Values.Add("MobileNo", TextBox3.Text);
+            Response.Cookies.Add(cookie1);
+            Label1.Text = "Cookie Added";
+        }
+    }
+}
