a5574bdc0465df0988f7cfbbb0273010fec8838b
PrathameshPVM <95204156+PrathameshPVM@users.noreply.github.com>
2022-03-23 09:10:45 +0530

    Add files via upload

--- home.aspx.cs
+++ home.aspx.cs
@@ -5,16 +5,11 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
-namespace Control1
+namespace themed_project2
 {
     public partial class home : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
-        {
-
-        }
-
-        protected void LinkButton1_Click(object sender, EventArgs e)
         {
 
         }
