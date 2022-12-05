6f66857d7d7fe007b7b52a4bfb4644afa3ab5f61
PrathameshPVM <95204156+PrathameshPVM@users.noreply.github.com>
2022-12-05 15:15:21 +0530

    dropdown id

---
+++ index1.aspx
@@ -0,0 +1,20 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="index1.aspx.cs" Inherits="dropdown_test.index1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:DropDownList ID="dept" runat="server"></asp:DropDownList>
+<%--            <asp:DropDownList ID="dept" runat="server"></asp:DropDownList>--%>
+            <asp:Button ID="save" runat="server" OnClick="save_Click" Text="Save" />
+            <asp:Label ID="Label1" runat="server"></asp:Label>
+
+        </div>
+    </form>
+</body>
+</html>
---
+++ index1.aspx.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace dropdown_test
+{
+    public partial class index1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+            if (!IsPostBack)
+            {
+                bind_drop();
+            }
+            
+        }
+        public void bind_drop()
+        {
+            SqlConnection con = new SqlConnection(System.Configuration.ConfigurationManager.ConnectionStrings["mycon"].ConnectionString);
+            SqlCommand cmd = new SqlCommand("select d_id,d_name FROM dept", con);
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+
+            dept.DataSource = ds;
+            dept.DataTextField = "d_name";
+            dept.DataValueField = "d_id";
+            dept.DataBind();
+
+            ds.Dispose();
+        }
+
+        protected void save_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = null;
+
+
+            try
+            {
+                con = new SqlConnection(ConfigurationManager.ConnectionStrings["mycon"].ToString());
+                SqlCommand cmd = new SqlCommand("ins_data", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                //cmd.Parameters.AddWithValue("@CustomerID", 0);    
+                cmd.Parameters.AddWithValue("@d_name",dept.SelectedItem.Text);
+                con.Open();
+                cmd.ExecuteNonQuery();
+                //cmd.ExecuteScalar().ToString();
+                Label1.Text = "Entered";
+
+            }
+            catch (Exception ee)
+            {
+                Label1.Text = ee.ToString();
+            }
+            finally
+            {
+                con.Close();
+            }
+            //Label1.Text = dept.SelectedItem.Text;
+        }
+    }
+}
